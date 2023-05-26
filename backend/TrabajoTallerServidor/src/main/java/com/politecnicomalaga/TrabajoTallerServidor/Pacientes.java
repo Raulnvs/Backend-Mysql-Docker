3d7b63b9a3dba8e5d3bdba67ca3efc5cfbdd646c
AdrianJS2009 <ajimsan2009@g.educaand.es>
2023-05-27 01:17:00 +0200

    Eliminadas referencias sin utilizar

--- backend/TrabajoTallerServidor/src/main/java/com/politecnicomalaga/TrabajoTallerServidor/Pacientes.java
+++ backend/TrabajoTallerServidor/src/main/java/com/politecnicomalaga/TrabajoTallerServidor/Pacientes.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
- */
+
 package com.politecnicomalaga.TrabajoTallerServidor;
 /*
 - Tendremos las siguientes "Call" o llamadas API Rest a nuestro HttpServlet:
