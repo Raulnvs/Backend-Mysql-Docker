7050e09b4c37b7840f98f8a4410df2d8d3ae9585
AdrianJS2009 <ajimsan2009@g.educaand.es>
2023-05-26 22:26:20 +0200

    Eliminadas referencias y imports sin utilizar.

--- backend/TrabajoTallerServidor/src/main/java/com/politecnicomalaga/TrabajoTallerServidor/BDAdaptador.java
+++ backend/TrabajoTallerServidor/src/main/java/com/politecnicomalaga/TrabajoTallerServidor/BDAdaptador.java
@@ -1,22 +1,13 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.politecnicomalaga.TrabajoTallerServidor;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.ArrayList;
 
 
-/**
- *
- * @author andres
- */
+
 public class BDAdaptador {
 
     private String sLastError;
