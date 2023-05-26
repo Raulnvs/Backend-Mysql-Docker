69592b86d8b4c8ac98b7916a6921e0579e976200
AdrianJS2009 <ajimsan2009@g.educaand.es>
2023-05-27 01:13:55 +0200

    Añadido mas comentarios y eliminado imports sin usar.

--- backend/TrabajoTallerServidor/src/main/java/com/politecnicomalaga/TrabajoTallerServidor/Pacientes.java
+++ backend/TrabajoTallerServidor/src/main/java/com/politecnicomalaga/TrabajoTallerServidor/Pacientes.java
@@ -23,16 +23,11 @@ de manera adecuada.
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.ArrayList;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- *
- * @author andres
- */
 public class Pacientes extends HttpServlet {
 
     /**
