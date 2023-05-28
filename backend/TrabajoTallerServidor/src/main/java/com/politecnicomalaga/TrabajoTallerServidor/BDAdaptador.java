7a5623d2d458fa0705c447ffdead4c7532725c4c
Raul Alvarez <ralvval0405@g.educaand.es>
2023-05-28 19:33:56 +0200

    Servlets (- cobrar)

--- backend/TrabajoTallerServidor/src/main/java/com/politecnicomalaga/TrabajoTallerServidor/BDAdaptador.java
+++ backend/TrabajoTallerServidor/src/main/java/com/politecnicomalaga/TrabajoTallerServidor/BDAdaptador.java
@@ -5,6 +5,7 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import com.google.gson.Gson;
 
 public class BDAdaptador {
 
@@ -19,7 +20,7 @@ public class BDAdaptador {
         // Establecemos la conexión
         Connection con = null;
         String dbDriver = "com.mysql.jdbc.Driver";
-        String dbURL = "jdbc:mysql://bbdd:3306/";
+        String dbURL = "jdbc:mysql://bbdd:3307/";
         // Nombre de la base de datos
         String dbName = "clinica_db";
         String dbUsername = "mecanico";
@@ -48,7 +49,7 @@ public class BDAdaptador {
             con = this.initDatabase();
 
             // st = con.createStatement();
-            ps = con.prepareStatement("select * from pacientes");
+            ps = con.prepareStatement("select * from Paciente");
 
             // ResultSet rs = st.executeQuery("select * from pacientes");
             ResultSet rs = ps.executeQuery();
@@ -106,7 +107,7 @@ public class BDAdaptador {
             con = this.initDatabase();
 
             // st = con.createStatement();
-            ps = con.prepareStatement("insert into pacientes (id,nombre,apellidos,telefono,email,dni,sFNac) values (?,?,?,?,?,?,?)");
+            ps = con.prepareStatement("insert into Paciente (id,nombre,apellidos,telefono,email,dni,sFNac) values (?,?,?,?,?,?,?)");
             ps.setString(1, String.valueOf((int) (Math.random() * 100000)));
             ps.setString(2, miPc.getsNombre());
             ps.setString(3, miPc.getsApellidos());
@@ -153,7 +154,7 @@ public class BDAdaptador {
             con = this.initDatabase();
 
             // st = con.createStatement();
-            ps = con.prepareStatement("select * from pacientes order by apellidos");
+            ps = con.prepareStatement("select * from Paciente order by apellidos");
 
             // ResultSet rs = st.executeQuery("select * from pacientes");
             ResultSet rs = ps.executeQuery();
@@ -196,4 +197,152 @@ public class BDAdaptador {
         if (sLastError.isEmpty()) return resultado;
         else return resultado + sLastError;
     }
+
+    public String getTodosTratamientos() {
+        String resultado = "";
+        String id_tratamiento, id_paciente, fecha, descripcion, coste;
+        Connection con = null;
+        Statement st = null;
+        PreparedStatement ps = null;
+        int iRows = 0;
+        try {
+            con = this.initDatabase();
+
+            // st = con.createStatement();
+            ps = con.prepareStatement("select * from  Tratamiento");
+
+            // ResultSet rs = st.executeQuery("select * from pacientes");
+            ResultSet rs = ps.executeQuery();
+
+            // Iteramos sobre el conjunto de resultados (resultset)
+            while (rs.next()) {
+                iRows++;
+                id_tratamiento = (rs.getString("id_tratamiento"));
+                id_paciente = rs.getString("id_paciente");
+                fecha = rs.getString("fecha");
+                descripcion = rs.getString("descripcion");
+                coste = rs.getString("coste");
+
+
+                // Guardamos los resultados
+                resultado += "<p>" + id_tratamiento + ";" +
+                        id_paciente + ";" +
+                        fecha + ";" +
+                        descripcion + ";" +
+                        coste + ";" + "</p>\n";
+            }
+        } catch (Exception e) {
+            sLastError = sLastError + "<p>Error accediendo a la base de datos (SELECT): " + e.getMessage() + "</p>";
+            e.printStackTrace();
+        } finally {
+            // Liberamos recursos. Cerramos la sentencia y la conexión
+            try {
+                if (st != null) st.close();
+                if (con != null) con.close();
+            } catch (Exception e) {
+                sLastError = sLastError + "<p>Error cerrando la base de datos: " + e.getMessage() + "</p>";
+                e.printStackTrace();
+
+            }
+        }
+        resultado += "\n<p>Rows recogidas: " + iRows + "</p>\n";
+        if (sLastError.isEmpty()) return resultado;
+        else return resultado + sLastError;
+    }
+
+    public String insertPacienteJson(String sJson) {
+        String resultado = "<p>Error al insertar</p>";
+        Connection con = null;
+
+        Gson gson = new Gson();
+        Paciente miPc = gson.fromJson(sJson, Paciente.class);
+
+        PreparedStatement ps = null;
+
+        try {
+            con = this.initDatabase();
+
+            // st = con.createStatement();
+            ps = con.prepareStatement("insert into Paciente (id,nombre,apellidos,telefono,email,dni,sFNac) values (?,?,?,?,?,?,?)");
+            ps.setString(1, String.valueOf((int) (Math.random() * 100000)));
+            ps.setString(2, miPc.getsNombre());
+            ps.setString(3, miPc.getsApellidos());
+            ps.setString(4, miPc.getsTelefono());
+            ps.setString(5, miPc.getsEmail());
+            ps.setString(6, miPc.getsDni());
+            ps.setString(7, miPc.getsFNac());
+
+            if (ps.executeUpdate() != 0)
+                resultado = "<p>Paciente insertado correctamente</p>";
+            else
+                resultado = "<p>Algo ha salido mal con la sentencia Insert Pacientes</p>";
+            // En este caso es una orden hacia la base de datos, y no tenemos
+            // ResultSet para iterar, puede ir bien o mal, nada más que hacer aquí
+
+        } catch (Exception e) {
+            sLastError = sLastError + "<p>Error accediendo a la base de datos (INSERT): " + e.getMessage() + "</p>";
+            e.printStackTrace();
+        } finally {
+            // Liberamos recursos. Cerramos la sentencia y la conexión
+            try {
+                if (ps != null) ps.close();
+                if (con != null) con.close();
+            } catch (Exception e) {
+                sLastError = sLastError + "<p>Error cerrando la base de datos: " + e.getMessage() + "</p>";
+                e.printStackTrace();
+
+            }
+        }
+        if (sLastError.isEmpty()) return resultado;
+        else return resultado + sLastError;
+
+    }
+    public String insertTratamientoJson(String sJson) {
+        String resultado = "<p>Error al insertar</p>";
+        Connection con = null;
+
+        Gson gson = new Gson();
+        Tratamiento miTr = gson.fromJson(sJson, Tratamiento.class);
+
+        PreparedStatement ps = null;
+
+        try {
+            con = this.initDatabase();
+
+            // st = con.createStatement();
+            ps = con.prepareStatement("insert into Tratamiento (id_tratamiento,id_paciente,fecha,descripcion,coste) values (?,?,?,?,?)");
+            ps.setString(1, String.valueOf((int) (Math.random() * 100000)));
+            ps.setString(2, miTr.getsCodigo());
+            ps.setString(3, miTr.getsFecha());
+            ps.setString(4, miTr.getsDescripcion());
+            ps.setFloat(5, miTr.getfPrecio());
+
+
+            if (ps.executeUpdate() != 0)
+                resultado = "<p>Tratamiento insertado correctamente</p>";
+            else
+                resultado = "<p>Algo ha salido mal con la sentencia Insert Tratamiento</p>";
+            // En este caso es una orden hacia la base de datos, y no tenemos
+            // ResultSet para iterar, puede ir bien o mal, nada más que hacer aquí
+
+        } catch (Exception e) {
+            sLastError = sLastError + "<p>Error accediendo a la base de datos (INSERT): " + e.getMessage() + "</p>";
+            e.printStackTrace();
+        } finally {
+            // Liberamos recursos. Cerramos la sentencia y la conexión
+            try {
+                if (ps != null) ps.close();
+                if (con != null) con.close();
+            } catch (Exception e) {
+                sLastError = sLastError + "<p>Error cerrando la base de datos: " + e.getMessage() + "</p>";
+                e.printStackTrace();
+
+            }
+        }
+        if (sLastError.isEmpty()) return resultado;
+        else return resultado + sLastError;
+
+    }
+
 }
+
